ee93059a5dc2543a951a9011bdd160345f904727
p2p-rigid <xinbo.jiang@gmail.com>
2016-06-12 15:42:07 +1000

    Revert "added backend"
    This reverts commit d9f11471184701597a95f249b27c5ce2116ead00.

--- front/app/controllers/albumviewcontroller.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	function AlbumViewController($scope, $routeParams) {
-		$scope.album_name = $routeParams.album_name;
-	}
-	albumApp.controller("AlbumViewController", AlbumViewController);
-})();
